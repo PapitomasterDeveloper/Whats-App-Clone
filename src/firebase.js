17972c844aa160a9537307c240c063269e42685d
Miguel Angel Cavazos Reyes <miguel.cavazos@ad.infosys.com>
2020-09-12 16:04:11 -0500

    Firebase config

---
+++ src/firebase.js
@@ -0,0 +1,11 @@
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyA5FdLyXYDn_SaurSlP_-M_yiqbe_OXOEw",
+  authDomain: "whats-app-clone-ef31e.firebaseapp.com",
+  databaseURL: "https://whats-app-clone-ef31e.firebaseio.com",
+  projectId: "whats-app-clone-ef31e",
+  storageBucket: "whats-app-clone-ef31e.appspot.com",
+  messagingSenderId: "622665223541",
+  appId: "1:622665223541:web:212cecc0764f32ebadd73c",
+  measurementId: "G-Y61K01FMC2"
+};
